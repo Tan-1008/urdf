53a922a81eaa93f03252254e33eeb092c42692be
Tan-1008 <tansinha006@gmail.com>
2024-12-11 22:21:53 +0530

    Initial commit

---
+++ src/three_wheeled_robot/launch/urdf_display.launch.py
@@ -0,0 +1,38 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    package_name = 'three_wheeled_robot'
+    urdf_file_name = 'my_robot.urdf'
+    urdf_path = os.path.join(get_package_share_directory(package_name), 'urdf', urdf_file_name)
+    print(f"URDF Path: {urdf_path}")
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(urdf_path).read()}]
+        ),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen'
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
